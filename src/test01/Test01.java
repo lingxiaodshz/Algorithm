f6476ad42a899879b2133eb651135ca32aaf48ae
lingxiaodshz <lingxiaodshz@163.com>
2018-05-04 20:35:19 +0800

    给出两个整数a和b, 求他们的和, 但不能使用 + 等数学运算符

--- src/test01/Test01.java
+++ src/test01/Test01.java
@@ -1,7 +1,27 @@
 package test01;
 
+import com.sun.javafx.geom.AreaOp.AddOp;
+
+/**
+ * 给出两个整数a和b, 求他们的和, 但不能使用 + 等数学运算符。
+ */
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("test");
+		Test01 test01 = new Test01();
+		System.out.println(test01.add(1000, 100));
+	}
+
+	public int add(int a, int b) {
+		if (a == 0) {
+			return b;
+		}
+		if (b == 0) {
+			return a;
+		}
+
+		int sum, i;
+		i = a ^ b;
+		sum = (a & b) << 1;
+		return add(sum, i);
 	}
 }
