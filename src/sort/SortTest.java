49d10e8ee0d50707442af18d40563f9c63537214
lingxiaodshz <lingxiaodshz@163.com>
2018-06-28 21:16:13 +0800

    选择排序和冒泡排序

---
+++ src/sort/SortTest.java
@@ -0,0 +1,23 @@
+package sort;
+
+public class SortTest {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 9, 4, 3, 8, 6, 3 };
+
+		print(arr);
+		System.out.println();
+
+//		SortUtils.bubbleSort(arr);
+		SortUtils.selectSort(arr);
+		print(arr);
+
+	}
+
+	private static void print(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+
+		}
+	}
+}
---
+++ src/sort/SortUtils.java
@@ -0,0 +1,38 @@
+package sort;
+
+public class SortUtils {
+	// √∞≈›≈≈–Ú
+	public static void bubbleSort(int[] arr) {
+		if (arr == null || arr.length <= 1) {
+			return;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					swap(arr, j, j + 1);
+				}
+			}
+		}
+	}
+
+	// —°‘Ò≈≈–Ú
+	public static void selectSort(int[] arr) {
+		if (arr == null || arr.length <= 1) {
+			return;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					swap(arr, i, j);
+				}
+			}
+		}
+
+	}
+
+	private static void swap(int[] arr, int a, int b) {
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+}
