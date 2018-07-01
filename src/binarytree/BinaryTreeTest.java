91c99b5c03a34a402050530059d798a9a54c27f3
lingxiaodshz <lingxiaodshz@163.com>
2018-07-01 17:17:16 +0800

    BinaryTree findKey

--- src/binarytree/BinaryTreeTest.java
+++ src/binarytree/BinaryTreeTest.java
@@ -9,17 +9,18 @@ public class BinaryTreeTest {
 
 }
 
-class Node{
+class Node {
 	int value;
 	Node leftChildNode;
 	Node rightChildNode;
+
 	public Node(int value) {
 		super();
 		this.value = value;
 	}
 }
 
-class BinaryTree{
+class BinaryTree {
 	private Node root = null;
 
 	public BinaryTree(int value) {
@@ -28,47 +29,68 @@ class BinaryTree{
 		root.leftChildNode = null;
 		root.rightChildNode = null;
 	}
-	
-	//查找
+
+	// 查找 针对排列的二叉树
 	public Node findKey(int value) {
+		Node currentNode = root;
+		while (true) {
+			if (currentNode.value == value) {
+				return currentNode;
+			} else if (value < currentNode.value) {
+				currentNode = currentNode.leftChildNode;
+			} else if (value > currentNode.value) {
+				currentNode = currentNode.rightChildNode;
+			}
+			
+			if (currentNode == null) {
+				return null;
+			}
+		}
+	}
+
+	// 插入
+	public String insert(int value) {
 		return null;
 	}
-	 //插入
-    public String insert(int value) {
-    	return null;
-    }
-    //中序遍历递归操作
-    public void inOrderTraverse() {
-    	
-    }
-    //中序遍历非递归操作    
-    public void inOrderByStack() {
-    	
-    }
-    //前序遍历
-    public void preOrderTraverse() {
-    	
-    }
-    //前序遍历非递归操作
-    public void preOrderByStack() {
-    	
-    }
-    //后序遍历
-    public void postOrderTraverse() {
-    	
-    }
-    //后序遍历非递归操作
-    public void postOrderByStack() {
-    	
-    }
-    //得到最小(大)值
-    public int getMinValue() {
-    	return 0;
-    } 
-    //删除
-    public boolean delete(int value) {
-    	return false;
-    } 
 
-	
+	// 中序遍历递归操作
+	public void inOrderTraverse() {
+
+	}
+
+	// 中序遍历非递归操作
+	public void inOrderByStack() {
+
+	}
+
+	// 前序遍历
+	public void preOrderTraverse() {
+
+	}
+
+	// 前序遍历非递归操作
+	public void preOrderByStack() {
+
+	}
+
+	// 后序遍历
+	public void postOrderTraverse() {
+
+	}
+
+	// 后序遍历非递归操作
+	public void postOrderByStack() {
+
+	}
+
+	// 得到最小(大)值
+	public int getMinValue() {
+		return 0;
+	}
+
+	// 删除
+	public boolean delete(int value) {
+		return false;
+	}
+
 }
