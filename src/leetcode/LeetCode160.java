fdcb3ec25b445577fa94d9b927534107a715ec4b
lingxiaodshz <lingxiaodshz@163.com>
2020-05-09 16:17:47 +0800

    LeetCode160. 相交链表

---
+++ src/leetcode/LeetCode160.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+public class LeetCode160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode curA = headA, curB = headB;
+        while (curA != null || curB != null) {
+            if (curA == null) {
+                curA = headB;
+            }
+            if (curB == null) {
+                curB = headA;
+            }
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
+
+
