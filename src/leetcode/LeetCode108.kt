6203285577f3421683d2990702a31a87e784d208
lingxiaodshz <lingxiaodshz@163.com>
2020-07-22 10:45:58 +0800

    LeetCode108. 将有序数组转换为二叉搜索树

---
+++ src/leetcode/LeetCode108.kt
@@ -0,0 +1,31 @@
+package leetcode
+
+/*
+108. 将有序数组转换为二叉搜索树
+将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+示例:
+给定有序数组: [-10,-3,0,5,9],
+一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+ */
+
+fun sortedArrayToBST(nums: IntArray): TreeNode? {
+    return sorted(nums, 0, nums.size - 1)
+}
+
+fun sorted(nums: IntArray, left: Int, right: Int): TreeNode? {
+    if (left > right) {
+        return null
+    }
+    var mid = left + (right - left) / 2
+    var treeNode = TreeNode(nums[mid])
+
+    treeNode.left = sorted(nums, left, mid - 1)
+    treeNode.right = sorted(nums, mid + 1, right)
+    return treeNode
+}
